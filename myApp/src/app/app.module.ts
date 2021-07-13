fce37da83fc4164dee9a1a7a8adf01a685ee0f8d
Bryan Aguirre Tudon <bryan_tudon@outlook.com>
2021-07-13 12:47:26 -0500

    ':3'

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,27 +1,3 @@
-# Angular Curso
-
-Curso de Angular
-
-## Instalacion de Angular
-
-```bash
-npm install -g @angular/cli
-```
-
-## Creacion de un nuevo proyecto
-```bash
-ng new MyApp
-cd MyApp
-ng serve
-```
-## Recomendacion
-
-Cunado creamos un proyecto nuevo podemos crear una carpeta llamada components y vamos creando los componentes
-
-## App.modulo.ts
-
-Este archivo esta conformado por varias partes del codigo que se deven saber
-```bash
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -49,5 +25,3 @@ import { AppComponent } from './components/app/app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-```
